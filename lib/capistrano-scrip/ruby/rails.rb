27c09ab70c64f46ec75fab64599d7febf96af92f
vladimir.lyukov <v.lyukov@gmail.com>
2013-02-13 09:12:32 +0400

    Create database/config.yml symlink before deploy:finelize_update
    in ruby/rails recipe

--- lib/capistrano-scrip/ruby/rails.rb
+++ lib/capistrano-scrip/ruby/rails.rb
@@ -6,11 +6,20 @@ Capistrano::Configuration.instance.load do
       db_seed
       start
     end
+
     task :db_setup, :roles => :app do
       run "cd #{current_path}; bundle exec rake environment RAILS_ENV=#{rails_env} db:setup"
     end
+
     task :db_seed, :roles => :app do
       run "cd #{current_path}; bundle exec rake environment RAILS_ENV=#{rails_env} db:seed"
+    end
+  end
+
+  before "deploy:finalize_update" do
+    if exists? :database_config_path
+      run "rm -f #{release_path}/config/database.yml && " \
+        "ln -s #{database_config_path} #{release_path}/config/database.yml"
     end
   end
 end
